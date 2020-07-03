ba6d3bbc6ba6db354919165e75075df6bc064922
niuzilin <zilin.niu@net263.com>
2020-07-03 16:14:36 +0800

    ..

--- app/src/main/java/com/kotlin/panel/constant/Constant.kt
+++ app/src/main/java/com/kotlin/panel/constant/Constant.kt
@@ -4,13 +4,11 @@ object Constant {
 
 
     const val BASE_URL = "https://www.wanandroid.com/"
-
     const val LOGIN_KEY = "login"
     const val USERNAME_KEY = "username"
     const val PASSWORD_KEY = "password"
     const val TOKEN_KEY = "token"
     const val HAS_NETWORK_KEY = "has_network"
-
     const val TODO_NO = "todo_no"
     const val TODO_ADD = "todo_add"
     const val TODO_DONE = "todo_done"
